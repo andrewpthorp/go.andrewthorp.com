6107f24dccbc193345e9967505eeb26fefdc9d89
Andrew Thorp <andrewpthorp@gmail.com>
2014-07-28 15:58:43 -0500

    updated procfile, go fmt

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: andrewthorp
+web: ./andrewthorp
--- andrewthorp.go
+++ andrewthorp.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "github.com/go-martini/martini"
-  "github.com/martini-contrib/render"
- )
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
 
 func main() {
-  m := martini.Classic()
-  m.Use(martini.Static("app/assets/css"))
-  m.Use(render.Renderer(render.Options{Directory: "app/templates"}))
+	m := martini.Classic()
+	m.Use(martini.Static("app/assets/css"))
+	m.Use(render.Renderer(render.Options{Directory: "app/templates"}))
 
-  m.Get("/", func(r render.Render){
-    r.HTML(200, "index", nil)
-  })
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", nil)
+	})
 
-  m.Run()
+	m.Run()
 }
